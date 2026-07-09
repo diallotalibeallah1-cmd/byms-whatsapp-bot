222347e41bf87f43910af422dbd0047ac99cf559
Talibe <diallotalibeallah1@gmail.com>
2026-07-09 22:10:13 +0000

    Organisation des photos dans le dossier photos

---
+++ rename_watches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p photos
+mv 1000001193.jpg photos/curren.jpg
+mv 1000001191.jpg photos/curren-turquoise-alt.jpg
+mv 1000001192.jpg photos/curren-orange-alt.jpg
+mv 1000001975.jpg photos/montblanc.jpg
+mv 1000001996.jpg photos/tommy-hilfiger.jpg
+mv 1000002000.jpg photos/tommy-hilfiger-or-alt.jpg
+mv 1000002459.jpg photos/rolex.jpg
+mv 1000002460.jpg photos/rolex-or-alt.jpg
+mv 1000002461.jpg photos/rolex-daydate-alt.jpg
+mv 1000002465.jpg photos/rolex-datejust-alt.jpg
+echo "Photos renommées et déplacées dans photos/"
